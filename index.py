04967af5fc0d25672bcb7a3b8e2467c62ffa1148
AS-line <taibov.0321@mail.ru>
2018-02-04 18:03:51 +0300

    Easy

--- index.py
+++ index.py
@@ -6,8 +6,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == "POST":
+        index_name = request.form.get("name_html_athribut", "noname")
+        index_year = request.form.get('year_html_athribut', 1990)
+        index_male = request.form.get('male_html_athribut', False)
+        print(index_name, index_year, index_male)
+        Person.create(name=index_name, year=index_year, male=index_male)
+    persons = Person.select()
+    return render_template('home.html', persons=persons)
+
+
+@app.before_request
+def before_request():
+    db.connect()
+
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
